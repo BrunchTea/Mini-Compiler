31b8eb99a62a8429e3c96ae0cd94e50772413455
BrunchTea <ScreeperC@outlook.com>
2023-05-19 17:30:10 +0800

    Add Parsing

--- data.h
+++ data.h
@@ -4,6 +4,7 @@
 
 // Global variables
 
-extern_ int     Line;
-extern_ int	Putback;
-extern_ FILE	*Infile;
+extern_ int     	Line;
+extern_ int		Putback;
+extern_ FILE		*Infile;
+extern_ struct token	Token;
